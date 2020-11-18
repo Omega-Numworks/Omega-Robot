ec45ffd558d9dea1b6532d98c2a6a30b0f300682
Shadow15510 <antoine.royer31@orange.fr>
2020-11-18 17:39:16 +0100

    Reach the PEP8 standard and review the configuration file

--- main.py
+++ main.py
@@ -1,47 +1,33 @@
-# --------------------------------------------------
-# Imports
-# --------------------------------------------------
-
 import discord
+import json
 import commands.all.all_commands as all_cmd
 import commands.omega.omega_commands as omega_cmd
 import commands.omega_wtf.omega_wtf_commands as omega_wtf_cmd
 
-from config import (
-    PREFIX,
-    TOKEN, 
-    ID_OMEGA, 
-    ID_OMEGA_WTF)
+# Configuration
+with open("config.json", "r") as file:
+    config = json.load(file)
 
 omega_robot = discord.client()
 
-# --------------------------------------------------
 # Global variables
-# --------------------------------------------------
-
 __version__ = "under developpement"
 
-# --------------------------------------------------
 # Instances of bot's commands
-# --------------------------------------------------
-
 ALL_COMMANDS = all_cmd.AllCommands()
 OMEGA_COMMANDS = omega_cmd.OmegaCommands()
 OMEGA_WTF_COMMANDS = omega_wtf_cmd.OmegaWTFCommands()
 
-# --------------------------------------------------
-# Bot's events
-# --------------------------------------------------
 
+# Bot's events
 @omega_robot.event()
 async def on_message(message):
-    if message.author == omega_robot.user():
-        return None
+    if message.author.bot:
+        return
 
     if message.guild.id in (ID_OMEGA, ID_OMEGA_WTF):  # Both servers commands
-
         if message.guid.id == ID_COMEGA:  # Omega commands
             pass
-        
+
         elif message.guild.id == ID_OMEGA_WTF:  # Omega WTF commands
             pass
