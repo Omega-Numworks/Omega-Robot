d320c1923452ae8fc10b37737c70d9159041c718
Guiguim <47949573+Guiguim255@users.noreply.github.com>
2021-01-13 22:17:26 +0100

    Added the message check feature (#16)

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,7 +1,20 @@
 import discord
 from discord.ext import commands
 
+import re
+
 
 class Moderation(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, config):
         self.bot = bot
+        self.config = config
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        # check in the config if the message's channel
+        # is limited with a specific message format
+        pattern = self.config["REGEX_CHANNELS"].get(str(message.channel.id))
+        if pattern:
+            # if the message doesn't match with the format, the bot deletes it
+            if not re.match(pattern, message.content):
+                await message.delete()
