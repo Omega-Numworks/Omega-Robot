17d5fca5274e602df6aba1fcaa47fd6d1c91c509
Tim-ats-d <tim.arnouts@protonmail.com>
2021-03-20 12:11:58 +0100

    Remove whitespaces contains in blank line.

--- logs/logger.py
+++ logs/logger.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger("Omega-robot logger")
 logger.setLevel(logging.INFO)
- 
+
 formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
 file_handler = RotatingFileHandler("logs/logbooks/activity.log",
                                     maxBytes=5000000,
@@ -17,4 +17,3 @@ logger.addHandler(file_handler)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 logger.addHandler(stream_handler)
- 
