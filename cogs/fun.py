4d856f75691ddd2d9ec42a00edcb23ec16f329ea
Sha-chan <67319895+Shadow15510@users.noreply.github.com>
2021-07-17 18:07:03 +0200

    Add a first version of the apod implementation (#22)

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,7 @@
 import aiohttp
 
+from bs4 import BeautilfulSoup
+
 import discord
 from discord.ext import commands
 
@@ -34,25 +36,52 @@ class Fun(commands.Cog):
         embed = discord.Embed()
         embed.set_footer(text="powered by nekos.life")
 
-        # Make a request to the nekos.life api at the command's name endpoint
+        # Make a request to the nekos.life api at the command's name endpoint.
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://nekos.life/api/v2/img/{ctx.invoked_with}") as r:
                 if r.status == 200:
                     data = await r.json()
                 else:
-                    # If the request status is not 200, send an error embed
+                    # If the request status is not 200, send an error embed.
                     embed.title = "API error"
                     embed.description = "Sorry, a problem has occurred when trying to interact with the nekos.life API"
-                    return await ctx.send(embed)
+                    return await ctx.send(embed=embed)
 
-        # Place the nekos.life's gif in the embed
+        # Place the nekos.life's gif in the embed.
         embed.set_image(url=data["url"])
 
-        # Make a custom message based on the command's template message
+        # Make a custom message based on the command's template message.
         if ctx.author.id != member.id:
             embed.description = actions[ctx.invoked_with].format(author=ctx.author.name, target=member.name)
         else:
             embed.description = f"Aww, I see you are lonely, I will {ctx.invoked_with} you"
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def apod(self, ctx):
+        """
+        This command requests the APOD (image and text).
+        """
+        embed = discord.Embed(title="Astronomy Picture of the Day", color="")
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://apod.nasa.gov/apod/astropix.html") as r:
+                if r.status == 200:
+                    apod = await r.text
+                else:
+                    # If the request status is not 200, send an error embed.
+                    embed.description = "Sorry, a problem has occurred when trying to interact with the apod website"
+                    return await ctx.send(embed=embed)
+
+        # Collect informations.
+        apod = BeautilfulSoup(apod, features="html5lib")
+        img = f"https://apod.nasa.gov/apod/{apod.find_all("img")[0]["src"]}"
+        text = apod.find_all("p")[2].text
+
+        # Make the embed.
+        embed.description = "Each day a different image or photograph of our fascinating universe is featured."
+        embed.set_image(img)
+        embed.add_field(name="Explanation", value=text[18:])
 
         await ctx.send(embed=embed)
 
