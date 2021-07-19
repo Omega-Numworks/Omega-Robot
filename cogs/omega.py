529872b96226d7617c53eeaa1f6cd7d3cb948e76
Tim-ats-d <tim.arnouts@protonmail.com>
2021-07-19 14:44:32 +0200

    Code quality upgrade.

--- cogs/omega.py
+++ cogs/omega.py
@@ -1,9 +1,11 @@
-import aiohttp
+"""Contains cog related to omega."""
+
 import asyncio
 from datetime import datetime
 import re
 from typing import AsyncGenerator
 
+import aiohttp
 import discord
 from discord.ext import commands
 
@@ -21,24 +23,32 @@ async def get_github_issues(message: discord.Message) -> AsyncGenerator[dict, No
     async with aiohttp.ClientSession() as session:
         for i in matches:
             issue = i[0].strip("#e ")
-            repo = "numworks/epsilon" if "e" in i[0] else "omega-numworks/omega"
+            repo = ("numworks/epsilon"
+                    if "e" in i[0]
+                    else "omega-numworks/omega")
 
-            async with session.get(f"https://api.github.com/repos/{repo}/issues/{issue}") as r:
-                if r.status != 200:
-                    await message.channel.send(f"Erreur lors de la requête ({r.status})")
+            async with session.get(f"https://api.github.com/repos/{repo}/issues/{issue}") as response:
+                if response.status != 200:
+                    await message.channel.send("Erreur lors de la requête "
+                                               f"({response.status})")
                     return
-                yield await r.json()
+                yield await response.json()
 
 
 async def make_embed(data: dict) -> discord.Embed:
-    embed = discord.Embed(title=data["title"], url=data["html_url"], description=data["body"])
+    """Return a formatted ``discord.Embed`` from given data."""
+    embed = discord.Embed(title=data["title"],
+                          url=data["html_url"],
+                          description=data["body"])
 
-    # Truncate the description if it's above the maximum size
+    # Truncate the description if it's above the maximum size.
     if len(embed.description) > 2048:
         embed.description = embed.description[:2043] + "[...]"
 
     author = data["user"]
-    embed.set_author(name=author["login"], url=author["html_url"], icon_url=author["avatar_url"])
+    embed.set_author(name=author["login"],
+                     url=author["html_url"],
+                     icon_url=author["avatar_url"])
 
     additional_infos = []
 
@@ -50,16 +60,20 @@ async def make_embed(data: dict) -> discord.Embed:
         additional_infos.append(":arrows_clockwise: Pull request")
 
         async with aiohttp.ClientSession() as session:
-            async with session.get(pull_request["url"] + "/commits") as r:
-                commits_data = await r.json()
+            async with session.get(pull_request["url"] + "/commits") as response:
+                commits_data = await response.json()
 
-        # Format all commits data into strings
-        formatted = ["[`{}`]({}) {} - {}".format(commit['sha'][:7], commit['html_url'], commit['commit']['message'],
-                                                 commit['committer']['login']) for commit in commits_data]
+        # Format all commits data into strings.
+        formatted = ["[`{}`]({}) {} - {}".format(commit['sha'][:7],
+                                                 commit['html_url'],
+                                                 commit['commit']['message'],
+                                                 commit['committer']['login'])
+                     for commit in commits_data]
 
         result = "\n".join(formatted)
 
-        # If the result is over the field's value's max size, it truncates the result
+        # If the result is over the field's value's max size,
+        # it truncates the result.
         if len(result) > 1024:
             diff = len(result) - 1024 + 4
 
@@ -73,22 +87,31 @@ async def make_embed(data: dict) -> discord.Embed:
         embed.add_field(name="Commits", value=result)
 
     if data["comments"]:
-        additional_infos.append(f":speech_balloon: Comments : {data['comments']}")
+        additional_infos.append(":speech_balloon: Comments : "
+                                f"{data['comments']}")
 
     if data["state"] == "closed":
-        closed_at = datetime.strptime(data["closed_at"], "%Y-%m-%dT%H:%M:%SZ").strftime("%b. %d %H:%M %Y")
-        additional_infos.append(f":x: Closed by {data['closed_by']['login']} on {closed_at}")
+        closed_at = datetime.strptime(data["closed_at"],
+                                      "%Y-%m-%dT%H:%M:%SZ").strftime("%b. %d %H:%M %Y")
+        additional_infos.append(":x: Closed by "
+                                f"{data['closed_by']['login']} on "
+                                f"{closed_at}")
     elif data["state"] == "open":
         additional_infos.append(":white_check_mark: Open")
 
     if data["labels"]:
-        additional_infos.append(f":label: Labels: `{'` `'.join(i['name'] for i in data['labels'])}`")
-    embed.add_field(name="Additional informations", value="\n".join(additional_infos))
+        labels = '` `'.join(i['name'] for i in data['labels'])
+        additional_infos.append(f":label: Labels: `{labels}`")
+
+    embed.add_field(name="Additional informations",
+                    value="\n".join(additional_infos))
 
     return embed
 
 
 class Omega(commands.Cog):
+    """Contains commands related to omega."""
+
     def __init__(self, bot, config):
         self.bot = bot
         self.config = config
@@ -97,31 +120,36 @@ class Omega(commands.Cog):
     @commands.Cog.listener()
     @user_only()
     async def on_message(self, message):
-        # Check if the message has an issue identifier in it
+        """Check if the message has an issue identifier in it and
+        process message if this is the case.
+        """
         if re.search("(^| )#[0-9]+e?($| )", message.content):
             async for i in get_github_issues(message):
                 # Create an embed with the data from the issue
                 embed = await make_embed(i)
 
-                # Send the embed in a message, react to it and temporally store this message's id
+                # Send the embed in a message, react to it and temporally store
+                # this message's id.
                 issue_embed = await message.channel.send(embed=embed)
                 await issue_embed.add_reaction("🗑️")
                 self.issue_embeds[issue_embed.id] = 1
 
-                # After 60 seconds, it deletes it from the storage dictionary
+                # After 60 seconds, it deletes it from the storage dictionary.
                 await asyncio.sleep(60)
                 self.issue_embeds.pop(issue_embed.id)
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, reaction):
-        # Ignore bots
+        """Delete issue embed if a user reacts with trash emote."""
+        # Ignore bots.
         reaction_user = await self.bot.fetch_user(reaction.user_id)
         if reaction_user.bot:
             return
 
         # If the reaction is "🗑️" and on a message stored in issue_embeds,
-        # it deletes it on discord and in the storage dictionary
-        if reaction.emoji.name == "🗑️" and self.issue_embeds.pop(reaction.message_id, None):
+        # it deletes it on discord and in the storage dictionary.
+        if (reaction.emoji.name == "🗑️"
+                and self.issue_embeds.pop(reaction.message_id, None)):
             channel = self.bot.get_channel(reaction.channel_id)
             message = await channel.fetch_message(reaction.message_id)
             await message.delete()
